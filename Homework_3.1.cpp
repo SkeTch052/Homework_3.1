e30c39440a995339c52ee268df47a12269000387
SkeTch <sketch13rus@mail.ru>
2023-05-23 09:45:36 +0300

    Project completed

--- Homework_3.1.cpp
+++ Homework_3.1.cpp
@@ -1,20 +1,79 @@
-﻿// Homework_3.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <locale.h>
 
-#include <iostream>
+class Calculator {
+public:
+    double num1;
+    double num2;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+    double add() {
+        return num1 + num2;
+    }
+    double multiply() {
+        return num1 * num2;
+    }
+    double subtract_1_2() {
+        return num1 - num2;
+    }
+    double subtract_2_1() {
+        return num2 - num1;
+    }
+    double divide_1_2() {
+        return num1 / num2;
+    }
+    double divide_2_1() {
+        return num2 / num1;
+    }
+    bool set_num1(double num1) {
+        if (num1 != 0) {
+            this->num1 = num1;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    bool set_num2(double num2) {
+        if (num2 != 0) {
+            this->num2 = num2;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    double num1, num2;
+    Calculator bip_boop;
+
+    while (1) {
+        do {
+            std::cout << "Введите num1: ";
+            std::cin >> num1;
+            bip_boop.set_num1(num1);
+            if (bip_boop.set_num1(num1) == false) {
+                std::cout << "Неверный ввод!" << std::endl;
+            }
+        } while (bip_boop.set_num1(num1) == false);
+
+        do {
+            std::cout << "Введите num2: ";
+            std::cin >> num2;
+            bip_boop.set_num2(num2);
+            if (bip_boop.set_num2(num2) == false) {
+                std::cout << "Неверный ввод!" << std::endl;
+            }
+        } while (bip_boop.set_num2(num2) == false);
+
+        std::cout << "num1 + num2 = " << bip_boop.add() << std::endl;
+        std::cout << "num1 - num2 = " << bip_boop.subtract_1_2() << std::endl;
+        std::cout << "num2 - num1 = " << bip_boop.subtract_2_1() << std::endl;
+        std::cout << "num1 * num2 = " << bip_boop.multiply() << std::endl;
+        std::cout << "num1 / num2 = " << bip_boop.divide_1_2() << std::endl;
+        std::cout << "num2 / num1 = " << bip_boop.divide_2_1() << std::endl;
+    }
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
